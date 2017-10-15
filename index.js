2917b0ee6e83e9528339fd6c079253d204a0f879
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-15 23:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var recipes = {};
-
-function updateObjectWithKeyAndValue(object, key, value){
-  var newObject = Object.assign(object, {[key]: value});
-
-  return newObject
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key];
-
-  return object
+function Objects(){
+  var recipes = {};
 }

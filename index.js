3445e6bf4d231793ac226d150f2c4bad62389ddd
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-16 00:23:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
